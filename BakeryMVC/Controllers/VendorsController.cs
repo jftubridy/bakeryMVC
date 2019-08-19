2ff243ba36f03518e556eb1a1948d5ff456b2dfd
Joseph Tubridy <jftubridy@gmail.com>
2019-08-19 05:53:39 -0700

    fixed one of the routes but now part of my orders is broken

--- BakeryMVC/Controllers/VendorsController.cs
+++ BakeryMVC/Controllers/VendorsController.cs
@@ -5,53 +5,53 @@ using BakeryMVC.Models;
 
 namespace BakeryMVC.Controllers
 {
-     public class VendorsController : Controller
-  {
-
-    [HttpGet("/vendors")]
-    public ActionResult Index()
+    public class VendorsController : Controller
     {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
-    }
 
-    [HttpGet("/vendors/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
 
-    [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
-    {
-      Vendor newVendor = new Vendor(vendorName);
-      return RedirectToAction("Index");
-    }
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
 
-    [HttpGet("/vendors/{id}")]
-    public ActionResult Show(int id)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("orders", vendorOrders);
-      return View(model);
-    }
+        [HttpPost("/vendors")]
+        public ActionResult Create(string vendorName)
+        {
+            Vendor newVendor = new Vendor(vendorName);
+            return RedirectToAction("Index");
+        }
 
-    // This one creates new Orders within a given Category of Vendor, not new Vendors:
-    [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string orderDescription)
-    {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
-      foundVendor.AddOrder(newOrder);
-      List<Order> vendorOrders = foundVendor.Orders;
-      model.Add("orders", vendorOrders);
-      model.Add("vendor", foundVendor);
-      return View("Show", model);
-    }
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
 
-  }
+        // This one creates new Orders within a given Vendor of Vendor, not new Vendors:
+        [HttpPost("/vendors/{vendorId}/orders")]
+        public ActionResult Create(int vendorId, string orderDescription)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor foundVendor = Vendor.Find(vendorId);
+            Order newOrder = new Order(orderDescription);
+            foundVendor.AddOrder(newOrder);
+            List<Order> vendorOrders = foundVendor.Orders;
+            model.Add("orders", vendorOrders);
+            model.Add("vendor", foundVendor);
+            return View("Show", model);
+        }
+
+    }
 }
